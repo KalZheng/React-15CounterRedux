4e5db97eb47d23e724642005d8231cc0ffeef07b
Kal Zheng <kal.zheng@inspire-dt.com>
2024-01-23 15:25:17 +0800

    add auth slice, so this will have multiple slice usage and also how to access the  state in different slices.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,8 +5,8 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
   //this is a subscription mention at the begining, just that this hooks is easier
-  const counter = useSelector((state) => state.counter);
-  const show = useSelector((state) => state.showCounter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {

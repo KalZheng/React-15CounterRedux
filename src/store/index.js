011916cb42640f7a09c14d56eed66f8801062e6b
Kal Zheng <kal.zheng@inspire-dt.com>
2024-01-23 14:19:54 +0800

    add a new state in redux and also new way to doing it

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,34 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
+  const newState = { ...state };
+
   if (action.type === "increment") {
-    return { counter: state.counter + 1 };
+    // beacause you are always need to return a new obejct (cannot change original state)
+    // return { counter: state.counter + 1, showCounter: state.showCounter };
+    // you can also create a new state and spread operate it and then return it for more flexibility
+    newState.counter = newState.counter + 1;
   }
   if (action.type === "increase") {
-    return { counter: state.counter + action.amount };
+    // return {
+    //   counter: state.counter + action.amount,
+    //   showCounter: state.showCounter,
+    // };
+    newState.counter = newState.counter + action.amount;
   }
   if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
+    // return { counter: state.counter - 1, showCounter: state.showCounter };
+    newState.counter = newState.counter - 1;
   }
-  return state;
+
+  if (action.type === "toggle") {
+    // return { showCounter: !state.showCounter, counter: state.counter };
+    newState.showCounter = !newState.showCounter;
+  }
+  // return state;
+  return newState;
 };
 
 const store = createStore(counterReducer);

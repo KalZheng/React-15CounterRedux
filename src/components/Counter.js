90dca387c6b8557d8443e7d87f0d4b5952044605
Kal Zheng <kal.zheng@inspire-dt.com>
2024-01-23 14:55:35 +0800

    convert use of redux to redux toolkit, easier to deal with redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 
+import { counterActions } from "../store/index";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
@@ -9,17 +10,21 @@ const Counter = () => {
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    // dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
   const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+    // dispatch({ type: "increase", amount: 5 });
+    dispatch(counterActions.increase(10));
   };
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    // dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: "toggle" });
+    // dispatch({ type: "toggle" });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,34 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action) => {
-  const newState = { ...state };
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      //here you can all state
+      // because in the background it create
+      // a copy of the old state and return it as new
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  if (action.type === "increment") {
-    // beacause you are always need to return a new obejct (cannot change original state)
-    // return { counter: state.counter + 1, showCounter: state.showCounter };
-    // you can also create a new state and spread operate it and then return it for more flexibility
-    newState.counter = newState.counter + 1;
-  }
-  if (action.type === "increase") {
-    // return {
-    //   counter: state.counter + action.amount,
-    //   showCounter: state.showCounter,
-    // };
-    newState.counter = newState.counter + action.amount;
-  }
-  if (action.type === "decrement") {
-    // return { counter: state.counter - 1, showCounter: state.showCounter };
-    newState.counter = newState.counter - 1;
-  }
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
-  if (action.type === "toggle") {
-    // return { showCounter: !state.showCounter, counter: state.counter };
-    newState.showCounter = !newState.showCounter;
-  }
-  // return state;
-  return newState;
-};
-
-const store = createStore(counterReducer);
+//we dont need to create individual action recepter as redux.
+export const counterActions = counterSlice.actions;
 
 export default store;
